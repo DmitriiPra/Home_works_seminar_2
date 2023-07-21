81075277c03cfb8cb6e65829b6d93e8b09bd048c
Pradim22 <792616@mail.ru>
2023-07-21 17:07:11 +0530

    Task 12 is ready

--- Hw_sem_2_task_12.py
+++ Hw_sem_2_task_12.py
@@ -1,7 +1,21 @@
-''' 
-Петя и Катя – брат и сестра. Петя – студент, а Катя –
-школьница. Петя помогает Кате по математике. Он задумывает два
-натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. Для
-этого Петя делает две подсказки. Он называет сумму этих чисел S и их
-произведение P. Помогите Кате отгадать задуманные Петей числа.
-'''
+""" 
+Задумано два натуральных числа X и Y (X,Y≤1000), надо их отгадать. 
+Для этого есть две подсказки: сумма этих чисел S и их произведение P.
+Программы должны отгадать задуманные числа.
+"""
+summa_numbers_S = int(input("введите значение суммы чисел X + Y = "))
+product_numbers_P = int(input("введите значение произведения чисел X * Y = "))
+
+if summa_numbers_S <= 0 or product_numbers_P <= 0:
+    print("Введены неверные значения")
+else:
+    first_term, second_term, res = 0, 0, 0
+    for i in range(1, int(summa_numbers_S / 2)):
+        first_term = i
+        second_term = summa_numbers_S - first_term
+        # print (first_term, second_term)
+        if first_term * second_term == product_numbers_P:
+            res = 1
+            print(f"задуманные числа: {first_term} и {second_term}")
+    if res == 0:
+        print("задачa не имеет решения")
